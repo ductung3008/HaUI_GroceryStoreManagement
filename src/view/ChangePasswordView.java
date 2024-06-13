aee1faeab7819cb941fcbf0ca8b5a34758176b7c
Tung <ductungltt@gmail.com>
2024-06-14 01:25:13 +0700

    add Home view

---
+++ src/view/ChangePasswordView.java
@@ -0,0 +1,162 @@
+package view;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import dao.UserDAO;
+import model.User;
+import util.FormUtils;
+import util.HashPassword;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+
+public class ChangePasswordView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel mainPanel;
+	private JTextField curPassField;
+	private JTextField rePassField;
+	private JTextField newPassField;
+	private UserDAO userDAO;
+
+	/**
+	 * Create the frame.
+	 */
+	public ChangePasswordView(User user) {
+		userDAO = new UserDAO();
+		Image icon = Toolkit.getDefaultToolkit().getImage(SignupView.class.getResource("/resources/logo.png"));
+		this.setIconImage(icon);
+		setResizable(false);
+		setTitle("Đổi mật khẩu");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		mainPanel = new JPanel();
+		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(mainPanel);
+		mainPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ĐỔI MẬT KHẨU", SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblNewLabel.setBounds(0, 0, 434, 42);
+		mainPanel.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(41, 53, 329, 42);
+		mainPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel curPassLabel = new JLabel("Mật khẩu hiện tại");
+		curPassLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		curPassLabel.setBounds(10, 11, 116, 20);
+		panel.add(curPassLabel);
+		
+		curPassField = new JPasswordField();
+		curPassField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		curPassField.setBounds(125, 0, 204, 42);
+		curPassField.setColumns(10);
+		curPassField.setBorder(BorderFactory.createCompoundBorder(curPassField.getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		panel.add(curPassField);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBounds(41, 159, 329, 42);
+		mainPanel.add(panel_1);
+		
+		JLabel rePassLabel = new JLabel("Nhập lại mật khẩu");
+		rePassLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		rePassLabel.setBounds(10, 11, 116, 20);
+		panel_1.add(rePassLabel);
+		
+		rePassField = new JPasswordField();
+		rePassField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		rePassField.setColumns(10);
+		rePassField.setBounds(125, 0, 204, 42);
+		rePassField.setBorder(BorderFactory.createCompoundBorder(rePassField.getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		panel_1.add(rePassField);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBounds(41, 106, 329, 42);
+		mainPanel.add(panel_2);
+		
+		JLabel newPassLabel = new JLabel("Mật khẩu mới");
+		newPassLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		newPassLabel.setBounds(10, 11, 116, 20);
+		panel_2.add(newPassLabel);
+		
+		newPassField = new JPasswordField();
+		newPassField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		newPassField.setColumns(10);
+		newPassField.setBounds(125, 0, 204, 42);
+		newPassField.setBorder(BorderFactory.createCompoundBorder(newPassField.getBorder(),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		panel_2.add(newPassField);
+		
+		JButton btnNewButton = new JButton("ĐỔI MẬT KHẨU");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!FormUtils.ValidateForm(mainPanel)) {
+					JOptionPane.showMessageDialog(ChangePasswordView.this, "Vui lòng nhập đầy đủ thông tin", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				String curPass = curPassField.getText();
+				String newPass = newPassField.getText();
+				String rePass = rePassField.getText();
+				
+				if (!user.getPassword().equals(HashPassword.hashPassword(curPass))) {
+					JOptionPane.showMessageDialog(ChangePasswordView.this, "Mật khẩu sai. Vui lòng thử lại.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				if (newPass.equals(curPass)) {
+					JOptionPane.showMessageDialog(ChangePasswordView.this, "Mật khẩu mới không được trùng mật khẩu cũ.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				if (!newPass.equals(rePass)) {
+					JOptionPane.showMessageDialog(ChangePasswordView.this, "Mật khẩu không khớp. Vui lòng nhập lại.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				user.setPassword(HashPassword.hashPassword(newPass));
+				try {
+					userDAO.update(user);
+					JOptionPane.showMessageDialog(ChangePasswordView.this, "Thay đổi mật khẩu thành công.");
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+				
+				dispose();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setBounds(147, 212, 137, 38);
+		mainPanel.add(btnNewButton);
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+}
---
+++ src/view/Home.java
@@ -0,0 +1,133 @@
+package view;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import model.User;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Home extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private User user;
+	private JPanel mainPanel;
+
+	public static void main(String[] args) {
+		new Home(new User());
+	}
+	
+	/**
+	 * Create the frame.
+	 */
+	public Home(User user) {
+		this.user = user;
+		Image icon = Toolkit.getDefaultToolkit().getImage(SignupView.class.getResource("/resources/logo.png"));
+		this.setIconImage(icon);
+		setResizable(false);
+		setTitle("HaUI Grocery Store Management");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		mainPanel = new JPanel();
+		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(mainPanel);
+		mainPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 348, 50);
+		mainPanel.add(panel);
+		panel.setLayout(null);
+		
+		String helloText = "Xin chào, " + user.getUsername() + "!";
+		JLabel helloLabel = new JLabel(helloText);
+		helloLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		helloLabel.setBounds(10, 11, 328, 28);
+		panel.add(helloLabel);
+		
+		JLabel titleLabel = new JLabel("Hệ thống quản lý tiệm tạp hóa HaUI", SwingConstants.CENTER);
+		titleLabel.setForeground(Color.ORANGE);
+		titleLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		titleLabel.setBounds(0, 41, 1264, 101);
+		mainPanel.add(titleLabel);
+		
+		JButton logoutBtn = new JButton("ĐĂNG XUẤT");
+		logoutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int option = JOptionPane.showConfirmDialog(Home.this, "Bạn sẽ đăng xuất khỏi hệ thống?");
+				if (option == 0) {
+					dispose();
+					new LoginView();
+				}
+			}
+		});
+		logoutBtn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		logoutBtn.setBounds(1128, 11, 126, 32);
+		mainPanel.add(logoutBtn);
+		
+		ImageIcon logoIcon = new ImageIcon(getClass().getResource("/resources/haui_logo.png"));
+		Image logoImg = logoIcon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+		logoIcon = new ImageIcon(logoImg);
+		JLabel logoLabel = new JLabel(logoIcon);
+		logoLabel.setBounds(10, 131, 1244, 150);
+		mainPanel.add(logoLabel);
+		
+		JButton btnNewButton = new JButton("Quản lý loại sản phẩm");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(365, 315, 200, 32);
+		mainPanel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Quản lý sản phẩm");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_1.setBounds(365, 390, 200, 32);
+		mainPanel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Quản lý hóa đơn");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_2.setBounds(365, 468, 200, 32);
+		mainPanel.add(btnNewButton_2);
+		
+		JButton verifyUserBtn = new JButton("Xác minh tài khoản");
+		verifyUserBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new VerifyUserView(user);
+			}
+		});
+		verifyUserBtn.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		verifyUserBtn.setBounds(699, 315, 200, 32);
+		mainPanel.add(verifyUserBtn);
+		
+		JButton btnNewButton_4 = new JButton("Thống kê");
+		btnNewButton_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton_4.setBounds(699, 390, 200, 32);
+		mainPanel.add(btnNewButton_4);
+		
+		JButton changePassBtn = new JButton("Đổi mật khẩu");
+		changePassBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new ChangePasswordView(user);
+			}
+		});
+		changePassBtn.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		changePassBtn.setBounds(699, 468, 200, 32);
+		mainPanel.add(changePassBtn);
+		
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+}
